1469fe1ea937d3fdfa8a908f83383b0e1252501a
Isaacdackey <isaacdackeyuniv@gmail.com>
2026-06-29 11:38:08 +0000

    chore: initial Spring Boot project

---
+++ src/main/java/BadWalletAPIPaymentService/BadWalletAPIPaymentService/BadWalletApiPaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package BadWalletAPIPaymentService.BadWalletAPIPaymentService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BadWalletApiPaymentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BadWalletApiPaymentServiceApplication.class, args);
+	}
+
+}
